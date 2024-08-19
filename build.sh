e046a6883305e9050a41acc57ed4b40b1690f1b2
shown zhang <shownstudio2u@gmail.com>
2024-08-20 07:34:11 +0800

    chore: 脚手架

--- build.sh
+++ build.sh
@@ -3,7 +3,4 @@ echo "Cleaning up..."
 rm -rf build && rm -rf bin
 printf "\n\n"
 echo "Building library: "
-mkdir build && cd build && cmake .. && make
-cd ..
-printf "\n\n"
-echo "Build successful!"
+mkdir build && cd build && cmake .. && make && cd .. && printf "\n\n" && echo "Build successful!"
