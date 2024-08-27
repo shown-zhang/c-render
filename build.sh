f381daf3569791e8d2f703e1381c96afd5501126
shown zhang <shownstudio2u@gmail.com>
2024-08-27 08:02:49 +0800

    feat: 调试glfw + glad

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ rm -rf build && rm -rf bin
 printf "\n\n"
 echo "Building library: "
 mkdir build && cd build && cmake .. && make && cd .. && printf "\n\n" && echo "Build successful!"
+printf "\n\n" && echo "Start running the program: "
+./bin/C_Render
